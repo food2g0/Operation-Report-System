8f925083cf331d647e37fd1f763be09306292805
unknown <foodtogo763@gmail.com>
2026-06-10 08:59:29 +0800

    fix: remove invalid -RunWithoutNetwork parameter from New-ScheduledTaskSettingsSet

--- setup_palawan_migration_task.ps1
+++ setup_palawan_migration_task.ps1
@@ -50,7 +50,7 @@ $action = New-ScheduledTaskAction -Execute $batchFile -WorkingDirectory $scriptD
 Write-Host "Task action created: Execute $batchFile" -ForegroundColor Green
 
 # Create task settings
-$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunWithoutNetwork -MultipleInstancePolicy Parallel
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -MultipleInstancePolicy Parallel
 
 # Create the task
 try {
